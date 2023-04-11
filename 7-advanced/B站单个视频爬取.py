6de027d207cd4cd441d89c4ae5c41f879ac2dadd
Ethan <stayhungry134@gmail.com>
2023-04-11 12:43:47 +0800

    B站视频爬取

--- 7-advanced/B站单个视频爬取.py
+++ 7-advanced/B站单个视频爬取.py
@@ -8,45 +8,70 @@ Description: 用于爬取 B 站 单个视频（不像学习视频一样有好多
 
 # 导入模块
 import requests
-from lxml import etree
-import os
-import re
+import re  # 正则表达式
+import pprint
+import json
+import subprocess
 
 url = input("请输入想要爬取的视频网址:")
-
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
-    'Referer': 'https://www.bilibili.com/',
-    'cookie': 'b_ut=-1; i-wanna-go-back=-1; _uuid=6A2B3837-1212-8A63-CF6E-FB681474A4AA81724infoc; buvid3=18E88705-72DB-405A-9FA6-63834B69A957167623infoc; fingerprint=96cb9dce79132721f312f52c83b6ef9b; buvid_fp=18E88705-72DB-405A-9FA6-63834B69A957167623infoc; buvid_fp_plain=18E88705-72DB-405A-9FA6-63834B69A957167623infoc; SESSDATA=ba839c9b%2C1646954136%2C09226%2A91; bili_jct=191c97fd218a7a1b7bf86d92b113cab0; DedeUserID=472410137; DedeUserID__ckMd5=8821f17a497b0a0f; sid=jseodwh8; blackside_state=1; rpdid=|(J|)Rmkmu|k0J\'uYJuR|kJmu; CURRENT_BLACKGAP=0; LIVE_BUVID=AUTO8916323716089346; CURRENT_QUALITY=80; bp_video_offset_472410137=581305045925505624; CURRENT_FNVAL=976; PVID=2; innersign=1',
-    'origin': 'https://search.bilibili.com'
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+    "referer": "https://www.bilibili.com/",
 }
 
-response = requests.get(url, headers=headers).content.decode('utf-8')
+def send_request(url):
+    response = requests.get(url=url, headers=headers)
+    return response
 
-# 解析网页
-html = etree.HTML(response)
-# 视频名称
-video_name = html.xpath('//h1/span/text()')[0]
 
-# 提取 视频 音频 url
-url_str = html.xpath('//script[contains(text(),"window.__playinfo__")]/text()')[0]
-video_url = re.findall(r'"video":\[{"id":\d+,"baseUrl":"(.*?)",', url_str)[0]
-audio_url = re.findall(r'"audio":\[{"id":\d+,"baseUrl":"(.*?)",', url_str)[0]
+def get_video_data(html_data):
+    """解析视频数据"""
 
-# 爬取视频
-video = requests.get(url=video_url, headers=headers).content
-audio = requests.get(url=audio_url, headers=headers).content
+    # 提取视频的标题
+    title = re.findall('<span class="tit">(.*?)</span>', html_data)[0]
+    # print(title)
 
-# 存储视频
-with open('E:/video/bilibili/video.mp4', 'wb') as f:
-    f.write(video)
+    # 提取视频对应的json数据
+    json_data = re.findall('<script>window\.__playinfo__=(.*?)</script>', html_data)[0]
+    # print(json_data)  # json_data 字符串
+    json_data = json.loads(json_data)
+    pprint.pprint(json_data)
 
-with open('E:/video/bilibili/audio.mp3', 'wb') as f:
-    f.write(audio)
+    # 提取音频的url地址
+    audio_url = json_data['data']['dash']['audio'][0]['backupUrl'][0]
+    print('解析到的音频地址:', audio_url)
 
-# 改变工作目录
-os.chdir('E:/video/bilibili')
-# 合成视频
-os.system('ffmpeg -i "video.mp4" -i "audio.mp3" -c copy "{}.mp4"'.format(video_name))
-os.remove('video.mp4')
-os.remove('audio.mp3')
+    # 提取视频画面的url地址
+    video_url = json_data['data']['dash']['video'][0]['backupUrl'][0]
+    print('解析到的视频地址:', video_url)
+
+    video_data = [title, audio_url, video_url]
+    return video_data
+
+# # 改变工作目录
+# os.chdir('E:/video/bilibili')
+# # 合成视频
+# os.system('ffmpeg -i "video.mp4" -i "audio.mp3" -c copy "{}.mp4"'.format(video_name))
+# os.remove('video.mp4')
+# os.remove('audio.mp3')
+
+def save_data(file_name, audio_url, video_url):
+    # 请求数据
+    print('正在请求音频数据')
+    audio_data = send_request(audio_url).content
+    print('正在请求视频数据')
+    video_data = send_request(video_url).content
+    with open(file_name + '.mp3', mode='wb') as f:
+        f.write(audio_data)
+        print('正在保存音频数据')
+    with open(file_name + '.mp4', mode='wb') as f:
+        f.write(video_data)
+        print('正在保存视频数据')
+
+
+def merge_data(video_name):
+    print('视频合成开始:', video_name)
+    # ffmpeg -i video.mp4 -i audio.wav -c:v copy -c:a aac -strict experimental output.mp4
+    COMMAND = f'ffmpeg -i {video_name}.mp4 -i {video_name}.mp3 -c:v copy -c:a aac -strict experimental output.mp4'
+    subprocess.Popen(COMMAND, shell=True)
+    print('视频合成结束:', video_name)
